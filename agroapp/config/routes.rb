d856a2258c7d55e8048261fd2cd315200ffae2ef
agroappgit <searaver1@gmail.com>
2016-04-15 07:38:11 -0600

    Can now write and read from growernotes model. I continue to be a god, albeit a lesser one.

--- agroapp/config/routes.rb
+++ agroapp/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-get '/growernotes/notes' => 'growernotes#index'
+get '/growernotes/index' => 'growernotes#index'
+get '/growernotes/new' => 'growernotes#new'
+post 'growernotes' => 'growernotes#create'
+resources :growernotes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
