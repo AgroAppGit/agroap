0d44b3491312299dea6962289b81239c9441853c
AgroAppGit <searaver1@gmail.com>
2016-05-06 16:29:25 -0600

    Security works!

--- agroapp/app/controllers/secure_login_controller.rb
+++ agroapp/app/controllers/secure_login_controller.rb
@@ -8,7 +8,7 @@ class SecureLoginController < ApplicationController
 		@messages = Message.all.order('created_at DESC')
 		@farm_login = current_user.farm_login
 		@security = request.fullpath
-		redirect_to '/login' unless '/public/'+current_user.farm_login == @security || current_user.farm_login == "agro"
+		redirect_to '/login' unless '/app/public/'+current_user.farm_login == @security || current_user.farm_login == "agro"
 	end
 	def index
 		@messages = Message.all
