ddb2627f9f3c617bf2b61b22beb84ef0274d5a03
AgroAppGit <searaver1@gmail.com>
2017-04-04 09:54:00 -0600

    Reraked client db for logins.

--- agroapp/db/migrate/20160416135140_create_users.rb
+++ agroapp/db/migrate/20160416135140_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
 	t.string :farm_name
 	t.string :password_digest
+	t.string :calendar_id
       t.timestamps null: false
     end
   end
