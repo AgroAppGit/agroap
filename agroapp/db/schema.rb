4bfe26f0846518b793ef98540a486882da9b136c
AgroAppGit <searaver1@gmail.com>
2017-04-04 09:46:43 -0600

    Tried adding calendar field to user table, then added entry of it into client sign up.

--- agroapp/db/schema.rb
+++ agroapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417125545) do
+ActiveRecord::Schema.define(version: 20170404154317) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "grower"
